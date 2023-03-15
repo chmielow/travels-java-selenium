286e3881cd3b8eeb311a73084585bf80728c8071
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-15 20:21:30 +0100

    Create new project travels and set city name

---
+++ src/test/java/HotelSearch.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HotelSearch {
+
+    @Test
+    public void searchHotel() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+
+    }
+}
