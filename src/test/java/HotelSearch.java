24b951223d55c516fc44bedc3185e8a6909e65b6
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-17 20:31:16 +0100

    Set check in and check out.

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -20,6 +20,9 @@ public class HotelSearch {
         driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+        driver.findElement(By.name("checkin")).sendKeys("18/08/22");
+        driver.findElement(By.name("checkout")).sendKeys("23/08/22");
+
 
     }
 }
