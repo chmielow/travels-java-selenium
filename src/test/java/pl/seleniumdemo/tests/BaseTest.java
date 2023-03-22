2d74d6e5922e6e73f44d267530111c2ced8bf328
Marcin Chmielowiec <marcinchmielow@gmail.com>
2023-03-22 19:51:40 +0100

    Add PropertiesLoader class and config.properties

--- src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -5,14 +5,15 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.seleniumdemo.utils.DriverFactory;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
-        driver = DriverFactory.getDriver("chrome");
+    public void setUp() throws IOException {
+        driver = DriverFactory.getDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
--- src/test/java/pl/seleniumdemo/utils/DriverFactory.java
+++ src/test/java/pl/seleniumdemo/utils/DriverFactory.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.IOException;
+
 public class DriverFactory {
-    public static WebDriver getDriver(String name) {
+    public static WebDriver getDriver() throws IOException {
+        String name = PropertiesLoader.loadProperty("browser.name");
         if (name.equals("firefox")) {
             return new FirefoxDriver();
 
